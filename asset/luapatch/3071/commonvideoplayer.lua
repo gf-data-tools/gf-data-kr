7330232a4a0239207d6f6ebae31f918e27957738
ZeroRin <ZeroRin@users.noreply.github.com>
2025-08-29 14:21:35 +0000

    KR | 30699 | data 0d228ed | dabao 20250829_15_26

--- asset/luapatch/3071/commonvideoplayer.lua
+++ asset/luapatch/3071/commonvideoplayer.lua
@@ -1,8 +1,47 @@
 local util = require 'xlua.util'
 xlua.private_accessible(CS.CommonVideoPlayer)
 xlua.private_accessible(CS.CriWare.CriManaMovieControllerForUI)
+xlua.private_accessible(CS.GFCriServerWrapper)
 local SetBattleMode = function(self, order)
 	self.movieController.previousOnApplicationPauseStatus = true
 	self:SetBattleMode(order)
 end
+
+local OnApplicationFocus = function(self,flag)
+	if CS.Application.platform == CS.UnityEngine.RuntimePlatform.WindowsPlayer then
+		if not CS.ConfigData.soundBackground then
+			if not flag then
+				CS.CommonAudioController.SetBGMSoundVolume(0);
+				CS.CommonAudioController.SetVoiceSoundVolueme(0);
+				CS.CommonAudioController.SetSoundVolume(0);
+			else
+				CS.CommonAudioController.SetBGMSoundVolume(CS.ConfigData.BGMValue);
+				CS.CommonAudioController.SetVoiceSoundVolueme(CS.ConfigData.VoiceValue);
+				CS.CommonAudioController.SetSoundVolume(CS.ConfigData.EffectValue);
+			end
+		end
+		return;
+	end
+	self:OnApplicationFocus(flag);
+end
+
+local OnApplicationPause = function(self,flag)
+	if CS.Application.platform == CS.UnityEngine.RuntimePlatform.WindowsPlayer then
+		if not CS.ConfigData.soundBackground then
+			if flag then
+				CS.CommonAudioController.SetBGMSoundVolume(0);
+				CS.CommonAudioController.SetVoiceSoundVolueme(0);
+				CS.CommonAudioController.SetSoundVolume(0);
+			else
+				CS.CommonAudioController.SetBGMSoundVolume(CS.ConfigData.BGMValue);
+				CS.CommonAudioController.SetVoiceSoundVolueme(CS.ConfigData.VoiceValue);
+				CS.CommonAudioController.SetSoundVolume(CS.ConfigData.EffectValue);
+			end
+		end
+		return;
+	end
+	self:OnApplicationPause(flag);
+end
 util.hotfix_ex(CS.CommonVideoPlayer,'SetBattleMode',SetBattleMode)
+util.hotfix_ex(CS.GFCriServerWrapper,'OnApplicationFocus',OnApplicationFocus)
+util.hotfix_ex(CS.GFCriServerWrapper,'OnApplicationPause',OnApplicationPause)
